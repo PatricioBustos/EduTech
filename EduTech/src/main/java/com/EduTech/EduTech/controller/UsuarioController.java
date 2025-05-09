50667944e739225aad3e85a6c1fa9c2a0ce947f5
Melipilla <melipilla@duoc.acad>
2025-05-09 15:41:46 -0400

    comit avance

--- EduTech/src/main/java/com/EduTech/EduTech/controller/UsuarioController.java
+++ EduTech/src/main/java/com/EduTech/EduTech/controller/UsuarioController.java
@@ -2,10 +2,9 @@ package com.EduTech.EduTech.controller;
 
 import com.EduTech.EduTech.model.Usuario;
 import com.EduTech.EduTech.service.UsuarioService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,5 +15,35 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> listarUsuarios(){
+        return usuarioService.getUsuarios();
+    }
+
+    @PostMapping
+    public Usuario agregarUsuario(@RequestBody Usuario usuario){
+        return usuarioService.saveUsuario(usuario);
+    }
+
+    @GetMapping("{rut}")
+    public Usuario buscarUsuario(@PathVariable int rut){
+        return usuarioService.getUsuarioRut(rut);
+    }
+
+    @GetMapping("{correo}")
+    public Usuario buscarUsuario(@PathVariable String correo){
+        return usuarioService.getUsuarioCorreo(correo);
+    }
+
+    @PutMapping("{rut}")
+    public Usuario actualizarUsuario(@PathVariable int rut, @RequestBody Usuario usuario) {
+        return usuarioService.upadteUsuario(usuario);
+    }
+
+    @DeleteMapping("{rut}")
+    public String eliminarUsuario(@PathVariable int rut) {
+        return usuarioService.deleteUsuario(rut);
+    }
 
 }
