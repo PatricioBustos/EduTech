240aab7ec5e0cd1c152eac94e885c2d97fc31466
BenjaminVera99 <be.veram@duocuc.cl>
2025-06-19 20:39:32 -0400

    Swaggers Listos en los 3 models

--- EduTech/src/main/java/com/EduTech/EduTech/controller/SoporteController.java
+++ EduTech/src/main/java/com/EduTech/EduTech/controller/SoporteController.java
@@ -1,7 +1,14 @@
 package com.EduTech.EduTech.controller;
 
+import com.EduTech.EduTech.model.Curso;
 import com.EduTech.EduTech.model.Soporte;
 import com.EduTech.EduTech.service.SoporteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,35 +16,49 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/soportes")
+@Tag(name = "Soporte", description = "Operaciones relacionadas con los Tickets de Soporte")
 public class SoporteController {
 
     @Autowired
     private SoporteService soporteService;
 
     @GetMapping
+    @Operation(summary = "Obtener todos los Tickets", description = "Obtiene una lista de todas los Tickets")
     public List<Soporte> listaTickets() {
         return soporteService.getSoportes();
     }
 
     @PostMapping
+    @Operation(summary = "Agregar Tickets", description = "Agrega un Ticket")
     public Soporte agregarTicket(@RequestBody Soporte soporte) {
         return soporteService.saveSoporte(soporte);
     }
 
-    // Buscar soporte por id
     @GetMapping("/{idTicket}")
+    @Operation(summary = "Buscar Tickets", description = "Busca un Ticket mediante su ID")
     public Soporte buscarTicket(@PathVariable int idTicket) {
         return soporteService.getTicketId(idTicket);
     }
 
-    // Actualizar soporte (usando PUT y path correcto)
     @PutMapping("/{idTicket}")
+    @Operation(summary = "Actualizar Ticket", description = "Actualiza un Ticket existente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ticket actualizado correctamente!!",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Curso.class))),
+            @ApiResponse(responseCode = "404", description = "Ticket no encontrado :c")
+    })
     public Soporte actualizarTicket(@PathVariable int idTicket, @RequestBody Soporte soporte) {
         soporte.setIdTicket(idTicket);  // Asegurar que el id venga del path
         return soporteService.updateTicket(soporte);
     }
 
     @DeleteMapping("/{idTicket}")
+    @Operation(summary = "Eliminar Ticket", description = "Elimina un Ticket por su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ticket Eliminado exitosamente!!"),
+            @ApiResponse(responseCode = "404", description = "Ticket no encontrado :c")
+    })
     public String eliminarTicket(@PathVariable int idTicket) {
         return soporteService.deleteTicket(idTicket);
     }
--- EduTech/src/main/java/com/EduTech/EduTech/controller/UsuarioController.java
+++ EduTech/src/main/java/com/EduTech/EduTech/controller/UsuarioController.java
@@ -1,7 +1,14 @@
 package com.EduTech.EduTech.controller;
 
+import com.EduTech.EduTech.model.Curso;
 import com.EduTech.EduTech.model.Usuario;
 import com.EduTech.EduTech.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
+@Tag(name = "Usuarios", description = "Operaciones relacionadas con los Usuarios")
 
 public class UsuarioController {
 
@@ -16,32 +24,48 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @GetMapping
+    @Operation(summary = "Obtener todos los Usuarios", description = "Obtiene una lista de todas los Usuarios")
     public List<Usuario> listarUsuarios(){
         return usuarioService.getUsuarios();
     }
 
     @PostMapping
+    @Operation(summary = "Agregar Usuarios", description = "Agrega un Usuario")
     public Usuario agregarUsuario(@RequestBody Usuario usuario){
         return usuarioService.saveUsuario(usuario);
     }
 
     @GetMapping("/rut/{rut}")
+    @Operation(summary = "Buscar Usuarios por RUT", description = "Busca un Usuario por su RUT")
     public Usuario buscarUsuarioPorRut(@PathVariable String rut){
         return usuarioService.getUsuarioRut(rut);
     }
 
     @GetMapping("/correo/{correo}")
+    @Operation(summary = "Buscar Usuario por Correo", description = "Busca un Usuario solo por su Correo")
     public Usuario buscarUsuarioPorCorreo(@PathVariable String correo){
         return usuarioService.getUsuarioCorreo(correo);
     }
 
     @PutMapping("/rut/{rut}")
+    @Operation(summary = "Actualizar Usuarios", description = "Actualiza un Usuario existente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario actualizado correctamente!!",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Curso.class))),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado :c")
+    })
     public Usuario actualizarUsuario(@PathVariable String rut, @RequestBody Usuario usuario) {
         return usuarioService.updateUsuario(usuario);
 
     }
 
     @DeleteMapping("/rut/{rut}")
+    @Operation(summary = "Eliminar Usuarios", description = "Elimina un Usuario por su RUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario Eliminado exitosamente!!"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado :c")
+    })
     public String eliminarUsuario(@PathVariable String rut) {
         return usuarioService.deleteUsuario(rut);
     }
